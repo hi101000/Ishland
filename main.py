e966ac0adfd4345b3daa7cef25e66587de4c700e
hi101000 <60079731+hi101000@users.noreply.github.com>
2023-09-19 09:36:07 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/login)
+def login():
+    return "<p>Hi</p>"
+
+@app.route("/login/<int:id>/<str:psswd>")
+def logging_in(id, psswd):
+    return "<p>Hola</p>"
